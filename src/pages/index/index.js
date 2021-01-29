0944c05fb45c643b59d257f8557b160b110eeca8
Simone <simone.sacchi@nubilaria.com>
2021-01-29 22:14:48 +0100

    first commit

---
+++ src/pages/index/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var App = require("src/pages/index/index.svelte").default;
+
+new App({
+    target: document.querySelector('#app')
+});
---
+++ src/pages/index/index.svelte
@@ -0,0 +1,36 @@
+<script>
+
+    console.log("ciao")
+
+</script>
+
+
+
+<header>nine dot task</header>
+
+<main>
+
+    <div class="dots-container">
+        <table>
+            <tr>
+                <td><span class="dot"></span></td>
+                <td><span class="dot"></span></td>
+                <td><span class="dot"></span></td>
+            </tr>
+            <tr>
+                <td><span class="dot"></span></td>
+                <td><span class="dot"></span></td>
+                <td><span class="dot"></span></td>
+            </tr>
+            <tr>
+                <td><span class="dot"></span></td>
+                <td><span class="dot"></span></td>
+                <td><span class="dot"></span></td>
+            </tr>
+        </table>
+    </div>
+
+</main>
+
+
+
